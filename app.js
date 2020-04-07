ce78d3c75d2892c403478dbcdc42b25c657c18c2
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-06 19:08:34 -0700

    add inquirer questions

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require("fs")
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html")
 
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRenderer")
 
+let quitSignal = false
+let members = []
+
+init()
+
+function init() {
+  askMemberQuestions().then(() => {
+    askToExit()
+  })
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+function askToExit() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "exit",
+        message: "Enter another member?",
+      },
+    ])
+    .then((ans) => {
+      console.log(ans)
+      if (!ans.exit) {
+        return
+      } else {
+        init()
+      }
+    })
+    .catch((err) => console.log(err))
+}
+
+function askMemberQuestions() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Members Name: ",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "Members Id: ",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "Members role: ",
+        choices: ["Manager", "Intern", "Engineer"],
+      },
+      /* Pass your questions in here */
+    ])
+    .then((answers) => {
+      members.push(answers)
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => console.log(error))
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
